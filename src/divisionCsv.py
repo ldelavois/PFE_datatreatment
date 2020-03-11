9ce8a000fa3edb5d746e661c9657f991cfae0cfc
ldelavois <ludovic.delavois@hotmail.fr>
2020-03-11 18:09:48 +0100

    add arguments in divisionCsv.py for line or dot plots

--- src/divisionCsv.py
+++ src/divisionCsv.py
@@ -7,8 +7,8 @@ import numpy as np
 import datetime
 import sys
 
+
 class Csv:
-    isBall = False
     def __init__(self):
 
         #create arguments
@@ -18,7 +18,7 @@ class Csv:
         self.commandType = self.parser.add_mutually_exclusive_group(required=True)
 
         self.commandType.add_argument("-f", "--file", type=str, nargs=1,
-                                 help="Select a file")
+                                 help="Select a csv file")
 
         self.parser.add_argument("-show", action="store_true", required = False,
                             help= "Show the plots figure")
@@ -26,19 +26,30 @@ class Csv:
         self.parser.add_argument("-save", action="store_true", required = False,
                                 help= "Save the plot figure in a .png file in the current path")
 
+        self.parser.add_argument("-all", action="store_true", required = False,
+                                help= "Plot the entire file")
+
+        self.parser.add_argument("-line", action="store_true", required = False,
+                                        help= "Plot lines between dots")
+
+
         self.args = self.parser.parse_args()
 
-        if self.args.file[0][-4:] == '.csv':
-            print("File selected: " + self.args.file[0])
-        else :
+        if self.args.file[0][-4:] != '.csv' :
             print("Error: Not a .csv file")
             sys.exit()
 
 
+
     def divisioncsv(self):
+        global isBall
+        global isState
 
         ##initialized variables
-        self.path = self.args.file[0]
+        if self.args.file:
+            self.path= self.args.file[0]
+            print(self.path)
+
         date = datetime.datetime.now()
         flag = 0
         tmp = []
@@ -50,18 +61,22 @@ class Csv:
         timeShoot = 0.0
         strDate = str(date.year) + '-' + str(date.month) + '-' + str(date.day) + '-' + str(date.hour) + 'h' + str(date.minute) + 'm' + str(date.second) + 's'
         newBallPos = False
+
         ##open the csv file to be treated
+
         input = open(self.path,'r')
         reader = csv.reader(input, delimiter=',')
         header = (next(reader)) #memorize the header and go the next line
 
         self.indexRobotX = header.index("pos_robotX")
         self.indexRobotY = header.index("pos_robotY")
-        self.indexApprochState = header.index("state")
-        if 'pos_ballX' in header:
-            isBall = True
-            self.indexBallX = header.index("pos_ballX")
-            self.indexBallY = header.index("pos_ballY")
+        if 'state' in header:
+            isState = True
+            self.indexApprochState = header.index("state")
+        # if 'pos_ballX' in header:
+        #     isBall = True
+        #     self.indexBallX = header.index("pos_ballX")
+        #     self.indexBallY = header.index("pos_ballY")
 
 
         ##loop for each line in the csv file
@@ -94,16 +109,23 @@ class Csv:
                             print("ligne au temps t ="+str(line[0])+ " ajoutée." )
                         flag = 1
 
-                if float(line[self.indexBallX]) != float(tmp[self.indexBallX]) or float(line[self.indexBallY])!= float(tmp[self.indexBallY]):
-                    newBallPos = True
+                if  isBall == True:
+                    if float(line[self.indexBallX]) != float(tmp[self.indexBallX]) or float(line[self.indexBallY])!= float(tmp[self.indexBallY]):
+                        newBallPos = True
 
 
                 ##copy the current line in the new csv file if diff posX > 0.03 and < 2
-                if abs(float(line[self.indexRobotX])-float(tmp[self.indexRobotX])) >= 0.03 and abs(float(line[1])-float(tmp[1])) < 2 or newBallPos == True:
+                if self.args.all:
                     writer = csv.writer(output, lineterminator='\n')
                     writer.writerow((line))
                     print("ligne au temps t ="+str(line[0])+ " ajoutée." )
                     tmp = line
+                else:
+                    if abs(float(line[self.indexRobotX])-float(tmp[self.indexRobotX])) >= 0.03 and abs(float(line[1])-float(tmp[1])) < 2 or newBallPos == True:
+                        writer = csv.writer(output, lineterminator='\n')
+                        writer.writerow((line))
+                        print("ligne au temps t ="+str(line[0])+ " ajoutée." )
+                        tmp = line
 
                 ##if diff PosX > 2, condition is ok to create a new csv file and copy the current line
                 if abs(float(line[self.indexRobotX])-float(tmp[self.indexRobotX])) >= 2 :
@@ -119,13 +141,16 @@ class Csv:
 
 
     def plots(self):
-
+        global isBall
+        global isState
+    ### plots
         if self.args.save or self.args.show:
-            ### plots
-
-            RobotX,RobotY,BallX,BallY = [],[],[],[]
-            RobotXStop, RobotXPlace, RobotXShoot = [],[],[]
-            RobotYStop, RobotYPlace, RobotYShoot = [],[],[]
+            RobotX,RobotY = [],[]
+            if isBall == True:
+                BallX,BallY = [],[]
+            if isState == True:
+                RobotXStop, RobotXPlace, RobotXShoot = [],[],[]
+                RobotYStop, RobotYPlace, RobotYShoot = [],[],[]
 
             maxX, minX, maxY, minY = [], [], [], []
             i=1
@@ -138,38 +163,48 @@ class Csv:
 
                 lines = [list(map(float, line)) for line in plots]
                 for line in lines:
-                    print(line)
-                    print("ligne au temps t ="+str(line[0])+ " ajoutée." )
-                    if line[self.indexApprochState] == 0:
-                        RobotXStop.append(line[self.indexRobotX])
-                        RobotYStop.append(line[self.indexRobotY])
+                    if isState == True :
+                        if line[self.indexApprochState] == 0:
+                            RobotXStop.append(line[self.indexRobotX])
+                            RobotYStop.append(line[self.indexRobotY])
 
-                    if line[self.indexApprochState] == 1:
-                        RobotXPlace.append(line[self.indexRobotX])
-                        RobotYPlace.append(line[self.indexRobotY])
+                        if line[self.indexApprochState] == 1:
+                            RobotXPlace.append(line[self.indexRobotX])
+                            RobotYPlace.append(line[self.indexRobotY])
+                            print (RobotXPlace)
 
-                    if line[self.indexApprochState] == 2:
-                        RobotXShoot.append(line[self.indexRobotX])
-                        RobotYShoot.append(line[self.indexRobotY])
+                        if line[self.indexApprochState] == 2:
+                            RobotXShoot.append(line[self.indexRobotX])
+                            RobotYShoot.append(line[self.indexRobotY])
 
                     RobotX.append(line[self.indexRobotX])
                     RobotY.append(line[self.indexRobotY])
-                    BallX.append(line[self.indexBallX])
-                    BallY.append(line[self.indexBallY])
+                    print (str(line[self.indexRobotX])+' , '+str(line[self.indexRobotX]))
 
-                    if line[self.indexRobotX] > line[self.indexBallX] :
+                    if isBall == True:
+                        BallX.append(line[self.indexBallX])
+                        BallY.append(line[self.indexBallY])
+
+
+                        if line[self.indexRobotX] > line[self.indexBallX] :
+                            maxX.append(line[self.indexRobotX])
+                            minX.append(line[self.indexBallX])
+                        else:
+                            maxX.append(line[self.indexBallX])
+                            minX.append(line[self.indexRobotX])
+
+                        if line[self.indexRobotY] > line[self.indexBallY] :
+                            maxY.append(line[self.indexRobotY])
+                            minY.append(line[self.indexBallY])
+                        else:
+                            maxY.append(line[self.indexBallY])
+                            minY.append(line[self.indexRobotY])
+                    else:
                         maxX.append(line[self.indexRobotX])
-                        minX.append(line[self.indexBallX])
-                    else:
-                        maxX.append(line[self.indexBallX])
-                        minX.append(line[self.indexRobotX])
-
-                    if line[self.indexRobotY] > line[self.indexBallY] :
                         maxY.append(line[self.indexRobotY])
-                        minY.append(line[self.indexBallY])
-                    else:
-                        maxY.append(line[self.indexBallY])
+                        minX.append(line[self.indexRobotX])
                         minY.append(line[self.indexRobotY])
+
                 i+=1
 
 
@@ -193,21 +228,28 @@ class Csv:
 
 
             #plt.plot(RobotXStop,RobotYStop,'g.',label ='Robot Stop')
-            plt.plot(RobotXPlace,RobotYPlace,'y.',label ='Robot Place')
-            plt.plot(RobotXShoot,RobotYShoot,'r.',label ='Robot Shoot')
-            plt.plot(BallX,BallY,'k.', label = 'Ball')
+            # if isState == True :
+            #     plt.plot(RobotXPlace,RobotYPlace,'y.',label ='Robot Place')
+            #     plt.plot(RobotXShoot,RobotYShoot,'r.',label ='Robot Shoot')
+            # else :
+            if self.args.line:
+                plt.plot(RobotX,RobotY,'r',label ='Robot')
+            else:
+                plt.plot(RobotX,RobotY,'r.',label ='Robot')
+
+            if isBall == True:
+                plt.plot(BallX,BallY,'k.', label = 'Ball')
             plt.legend()
 
         if self.args.save:
 
             plt.savefig(self.path[:-4]+'.png')
 
-
-
         if self.args.show:
             plt.show()
 
-
+isBall = False
+isState = False
 programme = Csv()
 programme.divisioncsv()
 programme.plots()
