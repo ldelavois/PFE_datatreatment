76ab5946c961625d73a993ad6fdd937ac58ee463
Ludovic DELAVOIS <33757570+ldelavois@users.noreply.github.com>
2020-03-27 15:52:26 +0100

    Update script.sh

--- src/script.sh
+++ src/script.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+#delete previous generated csv files and png files in the example approach repository
 cd ../data/approach_status_13h13_odom
 find . -name "*.png" | xargs rm
 find . -name "approach_status_13h13_odom-*.csv" | xargs rm
+
+#delete previous generated csv files and png files in the example position repository
 cd ../position-2020-03-13-13-26-05
 find . -name "*.png" | xargs rm
 find . -name "position-2020-03-13-13-26-05_v2-.csv" | xargs rm
+
+#launch the two functions that divide the two csv files then compare them
 cd ../../src
 python divisionCsv.py -f ../data/approach_status_13h13_odom/approach_status_13h13_odom.csv -save -line
 python divisionCsv.py -f ../data/position-2020-03-13-13-26-05/position-2020-03-13-13-26-05_v2.csv -save -line
