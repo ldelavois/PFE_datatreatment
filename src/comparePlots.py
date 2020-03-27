3a25755b0e7a82db94aac0ad288b9de9ac6dacfa
Ludovic DELAVOIS <33757570+ldelavois@users.noreply.github.com>
2020-03-27 15:49:50 +0100

    Update comparePlots.py

--- src/comparePlots.py
+++ src/comparePlots.py
@@ -25,7 +25,8 @@ class ComparePlots:
                                                 help= "Target position (x,y) and plot it")
 
         self.args = self.parsercompare.parse_args()
-
+        
+        ##check if the two inputs are csv files
         if self.args.files[0][-4:] != '.csv':
             print("Error: File 1 is not a .csv file")
             sys.exit()
@@ -33,14 +34,16 @@ class ComparePlots:
         if self.args.files[1][-4:] != '.csv':
             print("Error: File 2 is not a .csv file")
             sys.exit()
-
+            
+        #check if input 1 is approach and input 2 is position
         if 'approach' not in self.args.files[0] or 'position' not in self.args.files[1]:
             print("Error: File 1 must be approach file\n File 2 must be position file")
 
 
     def compareplots(self):
         self.paths=self.args.files
-        numFile = 1
+        
+        ##initialize the plot figure with labels and fixes axes
         plt.xlabel('x')
         plt.ylabel('y')
         axes = plt.axes()
@@ -49,20 +52,25 @@ class ComparePlots:
         axes.set_xlim([-0.5,3.5])
         axes.set_xticks([0,1,2,3])
         plt.title('Trajectories')
-
+        
+        ##for each file
         for path in self.paths:
             csvfile = open(path,'r')
             plots = csv.reader(csvfile, delimiter=',')
             header =next((plots))
             indexRobotX = header.index("pos_robotX")
             indexRobotY = header.index("pos_robotY")
+            
+            #check if there are ball positions
             if 'pos_ballX' in header:
                 self.indexBallX = header.index("pos_ballX")
                 self.indexBallY = header.index("pos_ballY")
                 BallX, BallY = [],[]
+                
             RobotX, RobotY = [],[]
             lines = [list(map(float, line)) for line in plots]
-
+            
+            ##add position for each line in the csv file
             for line in lines:
 
                 RobotX.append(line[indexRobotX])
@@ -70,6 +78,7 @@ class ComparePlots:
                 if 'pos_ballX' in header:
                     BallX.append(line[self.indexBallX])
                     BallY.append(line[self.indexBallY])
+            ##check the current treated file for the associated label
             if 'approach' in path:
                 plt.plot(RobotX,RobotY,label='Approach')
             else:
@@ -78,18 +87,12 @@ class ComparePlots:
                 #plt.plot(BallX,BallY,'.',label ='Ball')
         if self.args.target:
             plt.plot(self.args.target[0],self.args.target[1],'*', label = 'Target')
-
-
-
-
-
-
-            numFile+=1
+            
         plt.legend()
         plt.savefig(str(self.paths[0][:-4])+'-compare.png')
         plt.show()
 
 
-
+## main
 prgm = ComparePlots()
 prgm.compareplots()
